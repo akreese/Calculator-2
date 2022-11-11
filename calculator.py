55446f4b904c77f19c93b50677912f003115549f
akreese <76131466+akreese@users.noreply.github.com>
2022-11-10 20:04:14 -0800

    Added in input and tokens and if user quits

--- calculator.py
+++ calculator.py
@@ -5,3 +5,13 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+    calculations = input("Please enter your equation> ")
+    tokens = calculations.split(' ')
+    if tokens[0] == 'q':
+        print("You are now exiting")
+        break
+    equation = tokens[0]
+    num1 = tokens[1]
+
+
